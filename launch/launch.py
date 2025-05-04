2b722380988e4c474b29760a51eb79b6e56fa44f
creeper5820 <cqq1960180796@163.com>
2025-05-04 18:26:06 +0800

    update launch file

--- launch/launch.py
+++ launch/launch.py
@@ -1,13 +1,22 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-
 def generate_launch_description():
-
     launch = LaunchDescription()
-    
+
+    # 雷达驱动进程
+    # 由于代码因素，有时该进程无法正确响应 sigint 信号，请使用 ctrl + \ 强制关闭
+    lidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("livox_ros_driver2"), "/launch", "/msg_MID360_launch.py"]
+        )
+    )
+    launch.add_action(lidar)
+
     # 障碍地图生成进程
     obstacle = Node(
         package="rmcs_slam",
